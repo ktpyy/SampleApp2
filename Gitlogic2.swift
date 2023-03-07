2e1598e416b5993f24555f4734d81d12e3942b41
Takashima <s4khp4@gmail.com>
2023-03-07 22:58:17 +0900

    例外ケース実装

--- Gitlogic2.swift
+++ Gitlogic2.swift
@@ -13,28 +13,42 @@ class ElectricKettle {
     let limit: Int = 1000 // ml
     var water: Int? = nil
     
+    var kettleRemove = false {
+        didSet {
+            water = nil
+            print("取り外されました。")
+            timer?.invalidate()
+        }
+    }
+    
     func start() {
         timer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(countup), userInfo: nil, repeats: true)
-    }
+        }
     
     @objc func countup() {
         waterTemperature += 5
         print("\(waterTemperature)度です")
-        
         if limitTemperature <=  waterTemperature {
             print("沸騰しました。")
             timer?.invalidate()
         }
     }
-    func waterLimit(water: Int) {
-        water <= limit
-        print("\(water)ml入れました。")
-    }
     
+    func waterLimit(water: Int) {
+        if water <= 0 {
+            print("水が入っていません。")
+            timer?.invalidate()
+        } else if water <= limit {
+            print("\(water)ml入れました。")
+        } else if water > limit {
+            print("規定量を超えました。")
+            timer?.invalidate()
+        }
+    }
 }
 
 let kettle = ElectricKettle()
-
 kettle.start()
-kettle.waterLimit(water: 1000)
+kettle.waterLimit(water: )
+kettle.kettleRemove
 
